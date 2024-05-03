5d1ef8ac9b93ba1b879cf1f30502197ca6c8acbd
rezasaputra4250 <129853013+rezasaputra4250@users.noreply.github.com>
2024-05-03 18:41:31 +0700

    Create class.js

---
+++ js/class.js
@@ -0,0 +1,14 @@
+const kelasArray = [
+  "Kelas 1",
+  "Kelas 2",
+  "Kelas 3",
+  "Kelas 4",
+  "Kelas 5",
+  "Kelas 6",
+  "Kelas 7",
+  "Kelas 8",
+  "Kelas 9",
+  "Kelas 10",
+  "Kelas 11",
+  "Kelas 12",
+];
