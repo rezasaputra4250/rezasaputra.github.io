e282e58a035d6c3b5f9c2bf235d75e5573b9184d
rezasaputra4250 <129853013+rezasaputra4250@users.noreply.github.com>
2024-05-03 18:41:33 +0700

    Create educational-curricullum.js

---
+++ js/educational-curricullum.js
@@ -0,0 +1,15 @@
+const educationalCurriculum = [
+  "Rencana Pelajaran 1947",
+  "Rencana Pelajaran Terurai",
+  "Rencana Pendidikan 1964",
+  "Kurikulum 1968",
+  "Kurikulum 1975",
+  "Kurikulum 1984",
+  "Kurikulum 1994",
+  "Suplemen Kurikulum 1999",
+  "Kurikulum Berbasis Kompetensi (KBK) 2004",
+  "Kurikulum Tingkat Satuan Pendidikan (KTSP) 2006",
+  "Kurikulum 2013 (K-13)",
+  "Kurikulum 2013 Revisi (K-13 Revisi)",
+  "Kurikulum Merdeka",
+];
