b57d48d04c9dbc1e535c441d062a7c9f9a3af299
rezasaputra4250 <129853013+rezasaputra4250@users.noreply.github.com>
2024-05-03 12:48:38 +0700

    Create areas-of-expertise.js

---
+++ js/areas-of-expertise.js
@@ -0,0 +1,12 @@
+const bidangKeahlian = [
+    "Teknologi Konstruksi dan Bangunan",
+    "Teknologi Manufaktur dan Rekayasa",
+    "Energi dan Pertambangan",
+    "Teknologi Informasi",
+    "Kesehatan dan Pekerjaan Sosial",
+    "Agribisnis dan Agriteknologi",
+    "Kemaritiman",
+    "Bisnis dan Manajemen",
+    "Pariwisata",
+    "Seni dan Ekonomi Kreatif"
+];
